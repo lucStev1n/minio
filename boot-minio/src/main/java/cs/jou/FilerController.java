81558965c1826bba4d49b04ac46e6e944435fa86
eicen <lucheng@nsoc.com.cn>
2023-02-14 18:24:38 +0800

    feat: half of ls

--- boot-minio/src/main/java/cs/jou/FilerController.java
+++ boot-minio/src/main/java/cs/jou/FilerController.java
@@ -1,72 +1,75 @@
 package cs.jou;
 
 import cs.jou.support.AbstractOss;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("oss")
-@AllArgsConstructor
+import java.util.List;
+
+@RestController
+@RequestMapping("oss")
 public class FilerController {
 
     private final AbstractOss service;
 
-    // ls
-    @GetMapping
-    public Object ls() {
-        return null;
+    public FilerController(AbstractOss service) {
+        this.service = service;
     }
 
-    // 搜索 find
-    @GetMapping
-    public Object find() {
-        return null;
+    //todo ls
+    @GetMapping({"/{root}", "/", ""})
+    public Object ls(@PathVariable(required = false) String root) {
+        List result = service.ls(root, false);
+        return result;
     }
 
-    // 重命名 移动
+    //todo 搜索 find
+    @GetMapping("source/{regex}")
+    public Object find(@PathVariable String regex) {
+        return service.find("", regex);
+    }
+
+    //todo 重命名 移动
     @PatchMapping
     public Object mv() {
+        service.mv("source", "target");
         return null;
     }
 
-    // 删除 rm
+    //todo 删除 rm
     @DeleteMapping
     public Object rm() {
+        service.rm("");
         return null;
     }
 
-    // 创建 文件夹
-    @PostMapping
+    //todo 创建文件夹, 文件
+    @PostMapping("source")
     public Object mkdir() {
+        service.create("", null);
         return null;
     }
 
-    //    创建文件
-    @PostMapping
-    public Object touch() {
-        return null;
-    }
-
-    // 查看
-    @GetMapping
-    public Object vim() {
-        return null;
-    }
-
-    // 上传下载
-    @PostMapping
+    //todo 上传下载
+    @PostMapping("stream")
     public Object scp() {
         return null;
     }
 
-    // 取消上传下载
-    @DeleteMapping
+    //todo 查看
+    @GetMapping("cat")
+    public Object cat() {
+        return null;
+    }
+
+    //todo 取消上传下载
+    @DeleteMapping("cancel")
     public Object cancel() {
         return null;
     }
 
-    // 预览
-    @GetMapping
-    public Object cat() {
+    //todo 预览
+    @GetMapping("preview")
+    public Object preview() {
         return null;
     }
 }
--- boot-minio/src/main/java/cs/jou/support/AbstractOss.java
+++ boot-minio/src/main/java/cs/jou/support/AbstractOss.java
@@ -1,8 +1,7 @@
 package cs.jou.support;
 
-import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.messages.Item;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -11,14 +10,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public abstract class AbstractOss implements FileClient {
 
+    public static final String[] UNIT_NAMES = new String[]{"B", "KB", "MB", "GB", "TB", "PB", "EB"};
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractOss.class.getName());
 
     @Autowired
     private MinioClient client; // 七牛，华为，阿里，腾讯。。 aws gms
+
+    public static String format(long size) {
+        if (size <= 0L) {
+            return "0";
+        } else {
+            int digitGroups = Math.min(UNIT_NAMES.length - 1, (int) (Math.log10((double) size) / Math.log10(1024.0)));
+            return (new DecimalFormat("#,##0.##")).format((double) size / Math.pow(1024.0, (double) digitGroups)) + " " + UNIT_NAMES[digitGroups];
+        }
+    }
 
     @NotNull
     abstract protected String namespace();
@@ -39,8 +53,9 @@ public abstract class AbstractOss implements FileClient {
 
     }
 
-    public void find(String root, String regex) {
+    public List find(String root, String regex) {
 
+        return null;
     }
 
     public void preview() {
@@ -53,5 +68,41 @@ public abstract class AbstractOss implements FileClient {
 
     protected MinioClient Client() {
         return client;
+    }
+
+    @SneakyThrows
+    public List<?> ls(String root, boolean r) {
+        if (root == null) {
+            root = "/";
+        }
+
+        ArrayList<Item> result = new ArrayList<>();
+
+        for (Result<Item> item
+                : Client().listObjects(ListObjectsArgs.builder()
+                .bucket(namespace())
+                .prefix(root)
+                .recursive(false)
+                .build())) {
+            result.add(item.get());
+        }
+
+        return result.stream().map(item -> {
+            HashMap<String, Object> data = new HashMap<>();
+
+            data.put("name", item.objectName());
+            data.put("size", format(item.size()));
+            data.put("type", item.isDir() ? "folder" : "file");
+
+            return data;
+        }).collect(Collectors.toList());
+    }
+
+    public void mv(Object source, Object target) {
+
+    }
+
+    public Object rm(Object source) {
+        return null;
     }
 }
